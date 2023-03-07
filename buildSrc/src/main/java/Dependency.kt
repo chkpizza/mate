ca8d45b8649c1584c634f1e7d88f0c042511f132
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-03-07 19:24:09 +0900

    initialize project
    1. enabled Kotlin DSL
    2. add Dependency class to manage library version
    3. add Version class to manage app version

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,18 @@
+object AndroidX {
+    const val CORE = "androidx.core:core-ktx:1.7.0"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.8.0"
+}
+
+object Test {
+}
+
+object AndroidTest {
+    const val JUNIT4: String = "junit:junit:4.13.2"
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.5"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,5 @@
+object Version {
+    const val compileSdk = 33
+    const val targetSdk = 33
+    const val minSdk = 24
+}
