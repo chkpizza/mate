5f8963f8c87d5e62c9dfb2d25b24d738a99d0b1a
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2023-03-10 12:05:30 +0900

    [feature-common] add Constant class

---
+++ features/common/src/main/java/com/antique/common/Constant.kt
@@ -0,0 +1,11 @@
+package com.antique.common
+
+class Constant {
+    companion object {
+        const val UNINITIALIZED_VALUE = ""
+        const val UNINITIALIZED_CATEGORY = "카테고리를 선택해주세요"
+        const val CATEGORY_NODE = "Category"
+        const val USER_NODE = "User"
+        const val POST_NODE = "Post"
+    }
+}
